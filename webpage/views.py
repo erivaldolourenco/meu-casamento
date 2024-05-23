1c9bff9ea6d1c270acf19cf0f874d3ac01e717a8
Erivaldo Lourenco <erivaldo.mariano@pdcase.com.br>
2024-05-22 23:59:40 -0300

    Adicionando webapp e template do site.

---
+++ webpage/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.template import loader
+
+
+# Create your views here.
+def home(request):
+    template = loader.get_template("index.html")
+    context = {
+        "noivo": "Erivaldo",
+        "noiva": "Evely",
+        "data_casamento": "07 Dezembro",
+    }
+    return HttpResponse(template.render(context, request))
