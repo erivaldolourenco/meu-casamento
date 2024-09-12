d020850430d26cc2bbed0aa62203237da288d721
Erivaldo Lourenco <erivaldo.mariano@pdcase.com.br>
2024-09-11 23:34:03 -0300

    Adicina requirements.txt

--- listapresente/urls.py
+++ listapresente/urls.py
@@ -4,6 +4,6 @@ from listapresente import views
 
 urlpatterns = [
     path(r'', views.lista_de_presentes, name='lista_de_presente'),
-    path(r'/<id_presente>/presente', views.presente, name='presente'),
-    path(r'/<id_presente>/pagar', views.pagar, name='pagar'),
+    path(r'<id_presente>/presente', views.presente, name='presente'),
+    path(r'<id_presente>/pagar', views.pagar, name='pagar'),
 ]
