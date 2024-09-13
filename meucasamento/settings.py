6566caaa0bcb2b8196b7c2931f3948c3157decd5
Erivaldo Lourenco <erivaldo.mariano@pdcase.com.br>
2024-09-13 12:45:17 -0300

    adicionando STATIC_ROOT

--- meucasamento/settings.py
+++ meucasamento/settings.py
@@ -129,7 +129,7 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'webpage/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
