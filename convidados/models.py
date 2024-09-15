df200112b9ee9a9fe8ccb085427a1249552c0a53
Erivaldo Lourenco <erivaldo.mariano@pdcase.com.br>
2024-09-15 02:49:35 -0300

    Corrige erro do admin.

--- convidados/models.py
+++ convidados/models.py
@@ -29,4 +29,4 @@ class ConvidadoPresente(models.Model):
     status = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
-        return self.id_pagamento
+        return self.nome_convidado
